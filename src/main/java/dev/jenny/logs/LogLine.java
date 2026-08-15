2f925397c0a144e0333496322849c7b95bbe6137
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-08-16 01:07:43 +0200

    feat(log-line): add short format output for log lines

--- src/main/java/dev/jenny/logs/LogLine.java
+++ src/main/java/dev/jenny/logs/LogLine.java
@@ -3,14 +3,11 @@ package dev.jenny.logs;
 public class LogLine {
 
     private LogLevel logLevel;
+    private String message;
 
     public LogLine(String logLine) {
         parseLogLevel(logLine);
-    }
-
-    private void parseLogLevel(String logLine) {
-        String code = logLine.substring(1, 4);
-        this.logLevel = LogLevel.fromCode(code);
+        parseMessage(logLine);
     }
 
     public LogLevel getLogLevel() {
@@ -18,7 +15,15 @@ public class LogLine {
     }
 
     public String getOutputForShortLog() {
-        throw new UnsupportedOperationException("Please implement the getOutputForShortLog() method");
+        return String.format("%d:%s", logLevel.getEncodedValue(), message);
     }
 
+    private void parseLogLevel(String logLine) {
+        String code = logLine.substring(1, 4);
+        this.logLevel = LogLevel.fromCode(code);
+    }
+
+    private void parseMessage(String logLine) {
+        this.message = logLine.substring(logLine.indexOf(": ") + 2);
+    }
 }
